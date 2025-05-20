6324555ac3640fa905ac48d08a66be1f6a674dc5
Enrico Rossini <enrico.rossini.uk@live.com>
2025-05-20 18:03:48 +0100

    First connection

---
+++ RabbitMQConnection/RabbitMQConnection/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = await factory.CreateConnectionAsync())
+{
+    using (var channel = await connection.CreateChannelAsync())
+    {
+        await channel.QueueDeclareAsync("testqueue", true, false, false, null);
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += Consumer_ReceivedAsync;
+        await channel.BasicConsumeAsync("testqueue", true, consumer);
+
+        Console.ReadLine();
+    }
+}
+
+async Task<string?> Consumer_ReceivedAsync(object sender, BasicDeliverEventArgs ev)
+{
+    var body = ev.Body;
+    var content = Encoding.UTF8.GetString(body.ToArray());
+    Console.WriteLine(content);
+
+    return content;
+}
