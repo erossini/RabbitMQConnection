62ce19539a51160005680bd365a55054c474c4cc
Enrico Rossini <enrico.rossini.uk@live.com>
2025-05-21 01:19:36 +0100

    Add Connection_ConnectionShutdownAsync

--- RabbitMQConnection/RabbitMQConnection/Program.cs
+++ RabbitMQConnection/RabbitMQConnection/Program.cs
@@ -2,20 +2,77 @@
 using RabbitMQ.Client.Events;
 using System.Text;
 
-var factory = new ConnectionFactory() { HostName = "localhost" };
+ConnectionFactory factory;
+IConnection connection = null;
+IChannel channel = null;
+AsyncEventingBasicConsumer consumer;
 
-using (var connection = await factory.CreateConnectionAsync())
+factory = new ConnectionFactory() { HostName = "localhost" };
+await Reconnect();
+
+Console.ReadLine();
+
+async Task Connect()
 {
-    using (var channel = await connection.CreateChannelAsync())
+    connection = await factory.CreateConnectionAsync();
+    connection.ConnectionShutdownAsync += Connection_ConnectionShutdownAsync;
+
+    channel = await connection.CreateChannelAsync();
+    await channel.QueueDeclareAsync("testqueue", true, false, false, null);
+
+    consumer = new AsyncEventingBasicConsumer(channel);
+    consumer.ReceivedAsync += Consumer_ReceivedAsync;
+    await channel.BasicConsumeAsync("testqueue", true, consumer);
+}
+
+async Task CleanUp()
+{
+    try
     {
-        await channel.QueueDeclareAsync("testqueue", true, false, false, null);
+        if (channel != null && channel.IsOpen)
+        {
+            await channel.CloseAsync();
+            channel = null;
+        }
 
-        var consumer = new AsyncEventingBasicConsumer(channel);
-        consumer.ReceivedAsync += Consumer_ReceivedAsync;
-        await channel.BasicConsumeAsync("testqueue", true, consumer);
-
-        Console.ReadLine();
+        if(connection != null && connection.IsOpen)
+        {
+            await connection.CloseAsync();
+            connection = null;
+        }
     }
+    catch(IOException ex)
+    {
+        // Close() may throw an IOException if connection dies (handled by reconnect)
+    }
+}
+
+async Task Reconnect()
+{
+    await CleanUp();
+
+    var mres = new ManualResetEvent(false);
+
+    while(!mres.WaitOne(3000))
+    {
+        try
+        {
+            await Connect();
+
+            Console.WriteLine("Connected");
+            mres.Set();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Connection failed");
+        }
+    }
+}
+
+async Task Connection_ConnectionShutdownAsync(object sender, ShutdownEventArgs ev)
+{
+    Console.WriteLine("Connection lost");
+    await Reconnect();
 }
 
 async Task<string?> Consumer_ReceivedAsync(object sender, BasicDeliverEventArgs ev)
